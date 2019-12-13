4469aba74fd59582883958aef76d1877789f21c1
Andy Mabbutt <andy@pontbren.com>
2019-12-13 07:52:05 +0000

    spelling correction

--- src/prices.php
+++ src/prices.php
@@ -29,7 +29,7 @@ $page = basename($_SERVER["PHP_SELF"]);
 <h1>Prices &amp; Packages</h1>
 <p>At The Space we believe in a simple, transparent pricing policy offering our students as much flexibility and choice as possible.</p>
 <div class="stop-press">
-	<h1>Christimas Offer</h1>
+	<h1>Christmas Offer</h1>
 	<p class="c">10% discount and an extension of 7 days on all packages until December 27<sup>th</sup></p>	
 </div>
 <p>

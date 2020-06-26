4dc2a755c8f7b1071451d786fb2d6fdb6e4b6833
Andy Mabbutt <andy@pontbren.com>
2020-06-26 09:38:55 +0100

    Create katie-youers.php

---
+++ src/teachers/katie-youers.php
@@ -0,0 +1,11 @@
+<h1>Katie Youers</h1>
+<img src="images/teachers/bio-pages/katie-youers.jpg" alt="Katie Youers" class="w350fr">
+<p>Numerology originates from ancient Egypt and Babylonia. It is knowledge of the divine, mystical connection between events in the real world and number vibrations. These numbers consistently reveal accurate information about a person’s talents, abilities, personality, mission and lessons for an individual’s development and evolution in this lifetime, via their date of birth and name</p>.
+
+<p>A Personal Reading includes a comprehensive booklet containing your core numbers, other relevant numbers, a review of your previous year, your current year and a projection for the year ahead as well as a 2 hour personal consultation with me to help you understand and take action on your reading.</p>
+
+<p>Katie offers readings online on:</p>
+
+<p>Monday & Wednesday at 6pm to 8pm<br>
+	Thursday & Friday at 1pm to 3pm<br>
+	Cost: £7</p>
